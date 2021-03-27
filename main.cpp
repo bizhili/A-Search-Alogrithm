798b7b99028ce4019364ba22136eb680efd2fdd2
爱安敝之 <3067842904@qq.com>
2021-03-27 17:14:20 +0800

    upload

---
+++ main.cpp
@@ -0,0 +1,172 @@
+/***copyright @ bizhi***/
+/***2021/3/27***/
+#include "opencv_include_all.h"
+#include <iostream>
+#include <list>
+using namespace cv;
+using namespace std;
+
+/***initializing parameters***/
+#define s_len 800//square board pixels length
+#define random_line_length 3 //random barrier length
+#define random_line_num 200 //amount of random barrier
+#define random_seed 7//random seed
+/***struct of nodes***/
+typedef struct my_block
+{
+    unsigned short x;
+    unsigned short y;
+    unsigned short steps;
+    double score;
+}block;
+/***function:coloring a block***/
+void colorBox(Mat image,int x,int y,Scalar Color);
+/***function:judge node exiting in list or not***/
+int is_exist(list<block> table,int x,int y);
+/***function:check next step legally and push into list***/
+int check_and_do(int x,int y,list<block> &table_o,list<block> &table_t,int steps,uchar *copy_data,Mat image);
+/***parameter:calculate the block's length***/
+int box_num=s_len/19;
+int main()
+{
+    /***create an empty board to demonstrate procedure***/
+    Mat empty_p(s_len,s_len,CV_8UC3, Scalar(255,255,255));
+    /***create an empty small board to store information***/
+    Mat copy_p(box_num,box_num,CV_8UC1, Scalar(255));
+    uchar *copy_data = copy_p.data;
+    /***initialize board***/
+    for(int i=0;i<s_len;i+=19)
+        line(empty_p,Point(i,0),Point(i,s_len-1),Scalar(0, 0, 0),1, LINE_8);
+    for(int i=0;i<s_len;i+=19)
+        line(empty_p,Point(0,i),Point(s_len-1,i),Scalar(0, 0, 0),1, LINE_8);
+    //generate random numbers
+    theRNG().state = random_seed;
+    Mat1i row_rand(random_line_num,1);
+    Mat1i col_rand(random_line_num,1);
+    Mat1i dir_rand(random_line_num,1);
+    randu(row_rand, Scalar(2), Scalar(box_num-2));
+    randu(col_rand, Scalar(2), Scalar(box_num-2));
+    randu(dir_rand, Scalar(0), Scalar(2));
+    for (int i=0;i<random_line_num;i++)
+    {
+        int init_x=*col_rand[i],init_y=*row_rand[i];
+        if(*dir_rand[i])
+            for(int j=0;j<random_line_length;j++)
+            {
+                colorBox(empty_p,init_x+j,init_y,Scalar(150,150,150));
+                copy_data[box_num*(init_x+j)+init_y]=0;
+
+            }
+        else
+            for(int j=0;j<random_line_length;j++)
+            {
+                colorBox(empty_p,init_x,init_y+j,Scalar(150,150,150));
+                copy_data[box_num*init_x+init_y+j]=0;
+            }
+    }
+    namedWindow( "blank", CV_WINDOW_AUTOSIZE );
+    waitKey(1);
+    /***table_o:store open table;table_t:store terminal table***/
+    list<block>table_o,table_t;
+    list<block>::iterator iter,iter2;
+    int total_loop=0;
+    block temp_block1;
+    temp_block1.x=box_num-1;
+    temp_block1.y=box_num-1;
+    temp_block1.steps=0;
+    temp_block1.score=sqrt(33*33+33*33);
+    //put first node into open table
+    table_o.push_back(temp_block1);
+    while(table_o.size())
+    {
+        //find minimal score node in open table
+        double score_now=10000000.0f;
+        for(iter2=table_o.begin();iter2!=table_o.end();iter2++)
+        {
+            if(iter2->score<score_now)
+            {
+                score_now=iter2->score;
+                iter=iter2;
+            }
+        }
+        int x=iter->x,y=iter->y,steps=iter->steps;
+        //drop traced node in open table
+        table_o.erase(iter);
+        //put traced node into terminal table
+        table_t.push_front(*iter);
+        //color this block to show that this block has been traced
+        colorBox(empty_p,x,y,Scalar(0,250,0));
+        imshow("blank", empty_p);
+        waitKey(10);
+        //terminating loop condition
+        if(x==0 and y==0)
+            break;
+        //check 8 direction and put proper node into open table
+        check_and_do(x+1,y,table_o,table_t,steps,copy_data,empty_p);
+        check_and_do(x,y+1,table_o,table_t,steps,copy_data,empty_p);
+        check_and_do(x-1,y,table_o,table_t,steps,copy_data,empty_p);
+        check_and_do(x,y-1,table_o,table_t,steps,copy_data,empty_p);
+        check_and_do(x+1,y+1,table_o,table_t,steps,copy_data,empty_p);
+        check_and_do(x+1,y-1,table_o,table_t,steps,copy_data,empty_p);
+        check_and_do(x-1,y+1,table_o,table_t,steps,copy_data,empty_p);
+        check_and_do(x-1,y-1,table_o,table_t,steps,copy_data,empty_p);
+        ++total_loop;
+    }
+    //backtrace least cost road
+    int last_steps=table_t.begin()->steps+1,last_x=0,last_y=0,least_step=0;
+    for(iter2=table_t.begin();iter2!=table_t.end();iter2++)
+        if(last_steps>iter2->steps&&fabs(last_x-iter2->x)<=1&&fabs(last_y-iter2->y)<=1)
+        {
+            colorBox(empty_p,iter2->x,iter2->y,Scalar(0,0,255));
+            last_steps=iter2->steps;
+            last_x=iter2->x;
+            last_y=iter2->y;
+            ++least_step;
+        }
+    cout<<"total loops:"<<total_loop<<endl;
+    cout<<"least steps:"<<least_step<<endl;
+    imshow("blank", empty_p);
+	waitKey();
+	return 0;
+}
+void colorBox(Mat image,int x,int y,Scalar Color)
+{
+    rectangle(image,Point(x*19+1,y*19+1),Point(x*19+18,y*19+18),Color,CV_FILLED, 8, 0);
+}
+
+int is_exist(list<block> table,int x,int y)
+{
+    list<block>::iterator iter;
+    for(iter=table.begin();iter!=table.end();iter++)
+    {
+        if(iter->x==x && iter->y==y)
+            return 0;
+    }
+    return 1;
+}
+
+/***function:check next step legally and push into list***/
+int check_and_do(int x,int y,list<block> &table_o,list<block> &table_t,int steps,uchar *copy_data,Mat image)
+{
+    if(y>=0 &&y<=box_num-1 && x<=box_num-1 && x>=0 &&copy_data[box_num*(x)+y]!=0 && is_exist(table_o,x,y)&&is_exist(table_t,x,y))
+    {
+        block temp_block;
+        temp_block.x=x;
+        temp_block.y=y;
+        temp_block.steps=steps+1;
+        /***Here we can choose different heuristic functions***/
+        temp_block.score=sqrt(x*x+3*y*y)+steps+1;
+        //temp_block.score=sqrt(x*x+3*y*y)/1000+steps+1;
+        //temp_block.score=sqrt(x*x+3*y*y)*1000+steps+1;
+        //temp_block.score=fabs(x)+fabs(y)+steps+1;
+        //temp_block.score=fabs(x)+steps+1;
+        //temp_block.score=fabs(y-x)+steps+1;
+        //temp_block.score=fabs(y*x)+steps+1;
+        //temp_block.score=fabs(x*x)+steps+1;
+        //temp_block.score=fabs(x/(y+1))+steps+1;
+        table_o.push_back(temp_block);
+        colorBox(image,x,y,Scalar(120,255,255));
+    }
+    return 0;
+}
+
